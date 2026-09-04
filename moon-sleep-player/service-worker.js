52623302fb902d50a0ca48c88be1c6b91df4551e
xiaohaoddd <127035237+xiaohaoddd@users.noreply.github.com>
2026-09-04 20:16:21 +0800

    Delete moon-sleep-player directory
    删除

--- moon-sleep-player/service-worker.js
+++
@@ -1,1 +0,0 @@
-const CACHE='moon-sleep-v1';const FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
